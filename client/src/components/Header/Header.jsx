0908b2c45602067e109555589ae87f7e26c0f023
kofi arhin <kofiarhin69@gmail.com>
2026-04-02 02:31:21 +0100

    seetup project

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-center text-4xl p-8 text-bold">Prompt Hive</h1>
+    </div>
+  );
+};
+
+export default Header;
